80633ca8d7313ade5de72055e9759fb9d4f95bf1
daffakbar13 <daffa.akbar@sociolla.com>
2024-12-28 15:50:04 +0700

    fix: sort view histories

--- app/api/view-histories/route.ts
+++ app/api/view-histories/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
   const groups = await GuestGroupsModel()
   const groupList = await groups.find({}).toArray()
   const data = await viewHistories
-    .find({}, { sort: ['created_at', 'desc'] })
+    .find({}, { sort: ['created_at', -1] })
     .toArray()
     .then((res) =>
       res.map((e) => ({ ...e, group_name: groupList.find((g) => g._id.equals(e.group_id))?.name })),
